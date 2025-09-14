ee178fd4080d71e607d535ec89bf39c8725a8f1c
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-09-14 23:22:23 +0300

    add Js for cart and products

---
+++ JS/script.js
@@ -0,0 +1,71 @@
+let Products = [
+    {
+        imageUrl: "Images/The-Classic-Trench.png",
+        name: "The Classic Trench",
+        price: 350.00
+    },
+    {
+        imageUrl: "Images/Silk Scarf.png",
+        name: "Silk Scarf",
+        price: 120.00
+    },
+    {
+        imageUrl: "Images/bag.png",
+        name: "Leather Tote Bag",
+        price: 480.00
+    },
+    {
+        imageUrl: "Images/sweetshert.png",
+        name: "Cashmere Sweater",
+        price: 290.00
+    },
+    {
+        imageUrl: "Images/dress.png",
+        name: "Evening Dress",
+        price: 650.00
+    },
+    {
+        imageUrl: "Images/watch.png",
+        name: "Luxury Watch",
+        price: 900.00
+    },
+    {
+        imageUrl: "Images/shose1.png",
+        name: "Leather Shoes",
+        price: 220.00
+    },
+    {
+        imageUrl: "Images/hat.png",
+        name: "Classic Hat",
+        price: 80.00
+    },
+    {
+        imageUrl: "Images/perfum.png",
+        name: "Luxury Perfume",
+        price: 150.00
+    }
+];
+
+
+
+
+
+
+
+let cartItems = [
+    {
+        imageUrl: "Images/The-Classic-Trench.png",
+        name: "The Classic Trench",
+        price: 350.00
+    }
+]
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    
+
+    
+});
